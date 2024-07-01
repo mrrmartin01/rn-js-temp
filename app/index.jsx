cc8410308f6e8ecb5bd26053a33f3b75feeba16e
1 <1@mail.com>
2024-06-30 17:36:23 -0700

    necessary setups done

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,27 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-500">
-      <Text className = "text-6xl">If</Text>
-      <Text className = "text-4xl">you give up,</Text>
-      <Text className = "text-lg">just know you're a</Text>
-      <StatusBar style="auto" />
-      <Link href={"/profile"} className="text-sm">Bitch</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[38opx] w-full h-[300px]"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-
