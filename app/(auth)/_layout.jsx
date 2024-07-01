abcce56554455c5911f98c82432edb85bd75a2c1
1 <1@mail.com>
2024-07-01 15:10:31 -0700

    created a login and signup screens

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,6 +1,7 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
@@ -8,8 +9,9 @@ const AuthLayout = () => {
       <Stack>
         <Stack.Screen name="sign-in" options={{ headerShown: false }} />
         <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-        <StatusBar/>
       </Stack>
+      
+      <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
 };
